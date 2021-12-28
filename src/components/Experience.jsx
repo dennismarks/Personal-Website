613567e1a4c5bd2caa9f94913f325f51bbd4ea76
Dennis Markovchin <dennismarkovchin@gmail.com>
2021-12-28 12:31:35 +0200

    upd experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -78,13 +78,6 @@ export default function Experience() {
     <div>
       <GridContainer>
         <Title id="experience">Experience</Title>
-        <div></div>
-        <h3>
-          Currently on a 16-month internship as a Software Developer at Forma
-          AI. Programming teacher to high-school and entry-level university
-          students. Will be beginning the 4th year of Computer Science at the
-          University of Toronto in September 2021. Visual addict.
-        </h3>
       </GridContainer>
       {json.positions.map((p, index) => (
         <ExperienceInstance key={index} position={p}></ExperienceInstance>
